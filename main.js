5fdb124a4ad77aee50cc5663816827febc178b75
wozy <1297981152@qq.com>
2025-08-21 02:31:03 +0800

    cdn方式 引入 vue3 简单改造项目；添加 prettierrc 配置；丰富 折叠面板 子项内容

---
+++ main.js
@@ -0,0 +1,23 @@
+import { createApp, ref } from 'vue';
+import { personArr } from './data.js';
+
+const app = createApp({
+  setup() {
+    const foldList = ref(personArr());
+    const audioEnabled = ref(false);
+
+    console.log('🚀 ~ setup ~ foldList 👉', foldList.value);
+
+    const switchAudioEnabled = () => {
+      audioEnabled.value = !audioEnabled.value;
+    };
+
+    return {
+      foldList,
+      audioEnabled,
+      switchAudioEnabled,
+    };
+  },
+});
+
+app.mount('#app');
