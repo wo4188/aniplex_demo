9bb1848ad3cb09f2dcb8afa582c4d74d68ae3c73
wozy <1297981152@qq.com>
2025-08-31 23:13:56 +0800

    demo项目 "电子签名板-原生实现"，页面样式 完成

---
+++ demoCode.js
@@ -0,0 +1,20 @@
+// flat
+
+Array.prototype.myFlat = function (depth = 1) {
+  if (depth <= 0) return this.filter(() => true);
+
+  return this.reduce((acc, cur) => {
+    if (Array.isArray(cur)) {
+      acc.push(...cur.myFlat(depth - 1));
+    } else {
+      acc.push(cur);
+    }
+
+    return acc;
+  }, []);
+};
+
+const arr = [1, 2, , [[6, 7], , 8], 9];
+arr.myFlat(2); // [1, 2, 6, 7, 8, 9]
+
+// 🌈 filter/reduce 会跳过 稀疏数组的空槽 [1, ,2, , 3] => [1, 2, 3]
