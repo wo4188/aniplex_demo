e2dac9282c7a89edcddd766346985d3b13354026
wozy <1297981152@qq.com>
2025-09-04 21:23:47 +0800

    调整 公共基础路径，再次打包

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
